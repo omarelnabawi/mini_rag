74f5493d40b1d18b4b0c8b78fb92bd2c3bfb0faf
Nabawi <115591942+omarelnabawi@users.noreply.github.com>
2025-12-11 16:30:22 +0200

    return uploaded file_id

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -16,7 +16,7 @@ class DataController(BaseController):
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
             return False , ResponseSignal.FILE_SIZE_EXCEEDED
         return True ,ResponseSignal.FILE_UPLOAD_SUCCESS
-    def generate_unique_filename(self,original_filename: str,project_id: str):
+    def generate_unique_filepath(self,original_filename: str,project_id: str):
         
         random_filename=self.Generate_random_name()
         project_path=ProjectController().get_project_path(project_id)
@@ -24,18 +24,18 @@ class DataController(BaseController):
         cleaned_filename=self.get_clean_file_name(
             orig_filename=original_filename)
 
-        new_file_name=os.path.join(
+        new_file_path=os.path.join(
             project_path,
             random_filename+"_"+cleaned_filename
         )
 
-        while os.path.exists(new_file_name):
-            random_key=self.Generate_random_name()
-            new_file_name=os.path.join(
+        while os.path.exists(new_file_path):
+             random_filename=self.Generate_random_name()
+             new_file_path=os.path.join(
                 project_path,
-                random_key+"_"+cleaned_filename
+                random_filename+"_"+cleaned_filename
             )
-        return new_file_name
+        return new_file_path , random_filename+"_"+cleaned_filename
 
     def get_clean_file_name(self,orig_filename:str):
         # Remove special characters and spaces from the filename
